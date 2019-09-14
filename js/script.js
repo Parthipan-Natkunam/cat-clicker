4b27e4e26561e06dc31cfefd3a241eca366c06a2
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-09-15 01:07:23 +0530

    make the cat list load dynamically

--- js/script.js
+++ js/script.js
@@ -1,53 +1,52 @@
-const imageDir = '/img/';
+const imageDir = "/img/";
 const catsArray = [
-    {
-        id:0,
-        name: 'Phoebie',
-        clicks: 0,
-        image: `${imageDir}1.jpg`
-    },
-    {
-        id:1,
-        name: 'Chewie',
-        clicks: 0,
-        image: `${imageDir}2.jpg`
-    },
-    {
-        id:2,
-        name: 'Phoebie',
-        clicks: 0,
-        image: `${imageDir}1.jpg`
-    },
-    {
-        id:3,
-        name: 'Phoebie',
-        clicks: 0,
-        image: `${imageDir}1.jpg`
-    },
-    {
-        id:4,
-        name: 'Phoebie',
-        clicks: 0,
-        image: `${imageDir}1.jpg`
-    }
+  {
+    id: 0,
+    name: "Phoebie",
+    clicks: 0,
+    image: `${imageDir}1.jpg`
+  },
+  {
+    id: 1,
+    name: "Chewie",
+    clicks: 0,
+    image: `${imageDir}2.jpg`
+  },
+  {
+    id: 2,
+    name: "Garfield",
+    clicks: 0,
+    image: `${imageDir}3.jpg`
+  },
+  {
+    id: 3,
+    name: "Slade",
+    clicks: 0,
+    image: `${imageDir}4.jpg`
+  },
+  {
+    id: 4,
+    name: "Marvel",
+    clicks: 0,
+    image: `${imageDir}5.jpg`
+  }
 ];
 
-const loadCatList = () =>{
-
+const loadCatList = () => {
+  const catListDOM = document.getElementById("catList");
+  const listMarkup = catsArray.map(cat => {
+    return `<li class="cat-list__item">
+        <img src="${cat.image}" class="cat-list__thumbnail" alt="cat thumbnail" />
+        <span class="cat-list__name">
+          <b>${cat.name}</b>
+        </span>
+      </li>`;
+  });
+  catListDOM.innerHTML = listMarkup.join("");
 };
 
-const incrementCount = (id)=>{
-    const counterDOM = document.getElementById(`counter-${id}`);
-    counterDOM.innerText = ++catsArray[id].clicks;
+const initCatApp = () => {
+  loadCatList();
 };
 
-const initCatApp = ()=>{
-    const catDOMs = [];
-    for(let i =0 ; i<2; i++){
-        document.getElementById(`cat-name-${i}`).innerText = catsArray[i].name;
-        catDOMs.push(document.getElementById(`cat-container-${i}`));
-        catDOMs[i].addEventListener('click',(ev) => incrementCount(i));
-    }
-};
-
-window.addEventListener('load',initCatApp);
+window.addEventListener("load", initCatApp);
